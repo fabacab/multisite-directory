b8f5749f937c0cfdcfe1263de27c2a7c56e891aa
aurovrata <vrata@syllogic.in>
2016-03-22 16:15:41 +0530

    Boilerplate 3.0 refactor

---
+++ includes/class-taxonomy-for-network-sites-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://syllogic.in
+ * @since      1.0.0
+ *
+ * @package    Taxonomy_For_Network_Sites
+ * @subpackage Taxonomy_For_Network_Sites/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Taxonomy_For_Network_Sites
+ * @subpackage Taxonomy_For_Network_Sites/includes
+ * @author     Aurovrata V., May May <vrata@syllogic.in>
+ */
+class Taxonomy_For_Network_Sites_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
