f48b6878742cae7c7eb967ab027cd9f5b9147462
meitar <meitar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-30 06:52:43 +0000

    Custom post type and taxonomy
    git-svn-id: https://plugins.svn.wordpress.org/multisite-directory/trunk@1381734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/class-taxonomy-for-network-sites-admin.php
+++ admin/class-taxonomy-for-network-sites-admin.php
@@ -99,10 +99,143 @@ class Taxonomy_For_Network_Sites_Admin {
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/taxonomy-for-network-sites-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
-	
+	/**
+	 * Register the Network dashboard Sites sub-menu.
+	 *
+	 * This function is called using the hook 'network_admin_menu'
+	 *
+	 * @since    1.0.0
+	 */
 	public function add_category_submenu_to_sites(){
 		//add_submenu_page ( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '' )
-		add_submenu_page( 'sites.php', 'Sites Categories', 'Categories', 'manage_sites', '../edit-tags.php?taxonomy=category&post_type=post', '' );
+		add_submenu_page( 'sites.php', 'Site Categories', 'Categories', 'manage_sites', 'edit-tags.php?taxonomy='.T4NS_TAXONOMY.'&post_type='.T4NS_CUSTOM_POST, '' );
 	}
-
+	
+	/**
+	 * Register the custom taxonomy used for organising custom site post.
+	 *
+	 * This function is called using the hook 'init'
+	 *
+	 * @since    1.0.0
+	 */
+	public function regsiter_custom_taxonomy(){
+		$blog_id = get_current_blog_id();
+		if(1!=$blog_id) return; //we only want to register in the home site
+		$labels = array(
+			'name'                       => _x( 'Categories', 'Taxonomy General Name', 'taxonomy-for-network-sites' ),
+			'singular_name'              => _x( 'Category', 'Taxonomy Singular Name', 'taxonomy-for-network-sites' ),
+			'menu_name'                  => __( 'Categories', 'taxonomy-for-network-sites' ),
+			'all_items'                  => __( 'All Categories', 'taxonomy-for-network-sites' ),
+			'parent_item'                => __( 'Parent', 'taxonomy-for-network-sites' ),
+			'parent_item_colon'          => __( 'Parent:', 'taxonomy-for-network-sites' ),
+			'new_item_name'              => __( 'New Category Name', 'taxonomy-for-network-sites' ),
+			'add_new_item'               => __( 'Add New Category', 'taxonomy-for-network-sites' ),
+			'edit_item'                  => __( 'Edit Category', 'taxonomy-for-network-sites' ),
+			'update_item'                => __( 'Update Category', 'taxonomy-for-network-sites' ),
+			'view_item'                  => __( 'View Category', 'taxonomy-for-network-sites' ),
+			'separate_items_with_commas' => __( 'Separate Categories with commas', 'taxonomy-for-network-sites' ),
+			'add_or_remove_items'        => __( 'Add or remove Categories', 'taxonomy-for-network-sites' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'taxonomy-for-network-sites' ),
+			'popular_items'              => __( 'Popular Categories', 'taxonomy-for-network-sites' ),
+			'search_items'               => __( 'Search Categories', 'taxonomy-for-network-sites' ),
+			'not_found'                  => __( 'Not Found', 'taxonomy-for-network-sites' ),
+			'no_terms'                   => __( 'No Categories', 'taxonomy-for-network-sites' ),
+			'items_list'                 => __( 'categories list', 'taxonomy-for-network-sites' ),
+			'items_list_navigation'      => __( 'Categories list navigation', 'taxonomy-for-network-sites' ),
+		);
+		$capabilities = array(
+			'manage_terms'               => 'manage_network',
+			'edit_terms'                 => 'manage_network',
+			'delete_terms'               => 'manage_network',
+			'assign_terms'               => 'edit_posts',
+		);
+		$args = array(
+			'labels'                     => $labels,
+			'hierarchical'               => true,
+			'public'                     => true,
+			'show_ui'                    => true,
+			'show_admin_column'          => true,
+			'show_in_nav_menus'          => true,
+			'show_tagcloud'              => true,
+			'capabilities'               => $capabilities,
+			'update_count_callback'      => array($this, 'site_assigned_term_update'),
+		);
+		//register_taxonomy( $taxonomy, $object_type, $args );
+		register_taxonomy( T4NS_TAXONOMY, T4NS_CUSTOM_POST, $args );
+	}
+	/*
+	 *Call back function for when a site term update occurs
+	 *
+	 *This is a callback function
+	 */
+	public function site_assigned_term_update(){
+		//TODO: do we need to do anything here?
+	}
+	
+	/**
+	 * Register the custom post type for organising sites
+	 *
+	 * This function is called using the hook 'init'
+	 *
+	 * @since    1.0.0
+	 */
+	public function regsiter_custom_post(){
+		$labels = array(
+			'name'                  => _x( 'Sites', 'Post Type General Name', 'taxonomy-for-network-sites' ),
+			'singular_name'         => _x( 'Site', 'Post Type Singular Name', 'taxonomy-for-network-sites' ),
+			'menu_name'             => __( 'Sites', 'taxonomy-for-network-sites' ),
+			'name_admin_bar'        => __( 'Site', 'taxonomy-for-network-sites' ),
+			'archives'              => __( 'Site Archives', 'taxonomy-for-network-sites' ),
+			'parent_item_colon'     => __( 'Parent site:', 'taxonomy-for-network-sites' ),
+			'all_items'             => __( 'All sites', 'taxonomy-for-network-sites' ),
+			'add_new_item'          => __( 'Add New site', 'taxonomy-for-network-sites' ),
+			'add_new'               => __( 'Add New', 'taxonomy-for-network-sites' ),
+			'new_item'              => __( 'New Site', 'taxonomy-for-network-sites' ),
+			'edit_item'             => __( 'Edit Site', 'taxonomy-for-network-sites' ),
+			'update_item'           => __( 'Update Site', 'taxonomy-for-network-sites' ),
+			'view_item'             => __( 'View Site', 'taxonomy-for-network-sites' ),
+			'search_items'          => __( 'Search Site', 'taxonomy-for-network-sites' ),
+			'not_found'             => __( 'Not found', 'taxonomy-for-network-sites' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'taxonomy-for-network-sites' ),
+			'featured_image'        => __( 'Screenshot', 'taxonomy-for-network-sites' ),
+			'set_featured_image'    => __( 'Set Screenshot', 'taxonomy-for-network-sites' ),
+			'remove_featured_image' => __( 'Remove Screenshot', 'taxonomy-for-network-sites' ),
+			'use_featured_image'    => __( 'Use as Screenshot', 'taxonomy-for-network-sites' ),
+			'insert_into_item'      => __( 'Insert into Site', 'taxonomy-for-network-sites' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Site', 'taxonomy-for-network-sites' ),
+			'items_list'            => __( 'Sites list', 'taxonomy-for-network-sites' ),
+			'items_list_navigation' => __( 'Sites list navigation', 'taxonomy-for-network-sites' ),
+			'filter_items_list'     => __( 'Filter sites list', 'taxonomy-for-network-sites' ),
+		);
+		$capabilities = array(
+			'edit_post'             => 'manage_network',
+			'read_post'             => 'manage_network',
+			'delete_post'           => 'manage_network',
+			'edit_posts'            => 'manage_network',
+			'edit_others_posts'     => 'manage_network',
+			'publish_posts'         => 'manage_network',
+			'read_private_posts'    => 'manage_network',
+		);
+		$args = array(
+			'label'                 => __( 'Site', 'taxonomy-for-network-sites' ),
+			'description'           => __( 'Custom Post for Network site taxonomy', 'taxonomy-for-network-sites' ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'excerpt', 'thumbnail', 'custom-fields', 'page-attributes', ),
+			'taxonomies'            => array( T4NS_TAXONOMY ),
+			'hierarchical'          => true,
+			'public'                => false,
+			'show_ui'               => false,
+			'show_in_menu'          => false,
+			'menu_position'         => 5,
+			'show_in_admin_bar'     => false,
+			'show_in_nav_menus'     => false,
+			'can_export'            => false,
+			'has_archive'           => 'sites',
+			'exclude_from_search'   => true,
+			'publicly_queryable'    => false,
+			'rewrite'               => false,
+			'capabilities'          => $capabilities,
+		);
+		register_post_type( T4NS_CUSTOM_POST, $args );
+	}
 }
